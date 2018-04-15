262869e3a812f1196115faa558a3951f6dde6b0a
telin0411 <eggnome0411@gmail.com>
2018-04-14 17:42:19 -0700

    update .sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=1 --cuda --noise='add_noise' --outf=gan_outputs/ --mode='gan'
-#python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=0 --cuda --noise='add_noise' --outf=mmd_outputs/ --mode='mmd' --kernel='IMQ'
-python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=0 --cuda --noise='add_noise' --outf=mmd_outputs/ --mode='mmd' --kernel='RBF' --lr=1e-4 --LAMBDA=100 --niter=55 --e_pretrain
-python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=1 --cuda --noise='add_noise' --outf=gan_outputs/ --mode='gan' --lr=0.0003 --pz_scale=1 --LAMBDA=10 --niter=55 --e_pretrain
+# for mmd
+python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=0 --cuda --noise='gaussian' --outf=gan_outputs2/ --mode='gan' --lr=0.0003 --pz_scale=2 --LAMBDA=10 --niter=55 --e_pretrain
+
+# for gan
+python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=1 --cuda --noise='add_noise' --outf=gan_outputs/ --mode='gan' --lr=0.0003 --pz_scale=2 --LAMBDA=10 --niter=55 --input_normalize_sym
