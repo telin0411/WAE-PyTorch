9ee2678262ffc5cd2e6037153b4b3a326bf52333
telin0411 <eggnome0411@gmail.com>
2018-04-16 22:08:07 -0700

    updated after review

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=0 --cuda --noise='gaussian' --outf=mmd_outputs/ --mode='mmd' --lr=0.0003 --pz_scale=2 --LAMBDA=100 --niter=55 --kernel='IMQ' --e_pretrain
 
 # for gan
-python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=1 --cuda --noise='add_noise' --outf=gan_outputs/ --mode='gan' --lr=0.0003 --pz_scale=2 --LAMBDA=10 --niter=55 --input_normalize_sym
+python3 main.py --dataroot=data/CelebA --dataset='celebA' --gpu_id=0 --cuda --noise='add_noise' --outf=gan_outputs/ --mode='gan' --lr=0.0003 --pz_scale=2 --LAMBDA=10 --niter=55 --input_normalize_sym
